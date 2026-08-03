47d89aa3f50d8efcf076d95467fbf486f6a9250b
Duc Huy Nguyen <duchuytv2102@gmail.com>
2026-08-03 22:28:51 +0700

    chore: update TSConfig and configure dynamic LAN IP for development server access

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,37 @@
+// import type { NextConfig } from 'next';
+
+// const nextConfig: NextConfig = {
+//     allowedDevOrigins: ['192.168.2.3'],
+// };
+
+// export default nextConfig;
+
+
 import type { NextConfig } from 'next';
+import { PHASE_DEVELOPMENT_SERVER } from 'next/constants';
+import os from 'os';
 
-const nextConfig: NextConfig = {};
+function getLocalIPv4(): string | null {
+    const interfaces = os.networkInterfaces();
+    for (const name of Object.keys(interfaces)) {
+        for (const iface of interfaces[name]!) {
+            if (iface.family === 'IPv4' && !iface.internal) return iface.address;
+        }
+    }
+    return null;
+}
 
-export default nextConfig;
+export default (phase: typeof PHASE_DEVELOPMENT_SERVER) => {
+    const nextConfig: NextConfig = {
+
+    };
+
+    if (phase === PHASE_DEVELOPMENT_SERVER) {
+        const currentLanIP = getLocalIPv4();
+        if (currentLanIP) {
+            nextConfig.allowedDevOrigins = [currentLanIP];
+        }
+    }
+
+    return nextConfig;
+};
